979a2aec7e11aa3f3a0a90b642ef87879a80a375
Kawser Simanto <kawsersimanto@gmail.com>
2025-09-29 13:05:12 +0600

    updated combobox for full width

--- src/components/combobox/Combobox.tsx
+++ src/components/combobox/Combobox.tsx
@@ -55,7 +55,10 @@ export const Combobox = ({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[300px] p-0">
+      <PopoverContent
+        className="w-full min-w-[var(--radix-popover-trigger-width)] p-0"
+        align="start"
+      >
         <Command>
           <CommandInput placeholder={searchPlaceholder} />
           <CommandList>
